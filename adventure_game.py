39e681ad5a55bcbc448427dbd23abfd532dda8dd
Kendricka (Kay) Haywood <100977656+kayhaywood@users.noreply.github.com>
2022-05-24 21:24:40 -0400

    Add files via upload

---
+++ adventure_game.py
@@ -0,0 +1,29 @@
+#While hanging out with your friends in the mall, you stop at a box and enter a raffle 
+# to participate in a game show. 
+#You get a call days later saying you are the lucky raffle winner out of 1000s of people!
+#they give you the address for the taping of the game show and you and your friends hop in the car!
+# They tell you that you as the raffle winner have a choice of picking either door number 1 or 
+# number 2 for a chance to win the grand prize!
+#Which door would you like to chose?
+
+#player chooses door 1
+#Let's check what's behind door number 1! time delay
+#Congratulations! You won the grand prize! A vacation to Hawaii! #make a random component ie. different vacay places
+# France, Bora Bora, Florida
+
+#player chooses door 2
+#let's check what's behind door number 2! time delay
+#oh no! you picked the door with only a dollar! #make random choices ie cupcake, quarter
+#better luck next time!
+
+#if type something other than 1 or 2. 
+#say sorry i dont understand. pick door 1 or 2
+
+#would you like to play again? or exit?
+# type restart or exit
+
+#if something else say dont understand.
+#restart or exit
+
+
+
